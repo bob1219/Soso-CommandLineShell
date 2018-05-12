3dba6949b3d98f4f3847905844a6094cd484c35d
吉田大輝 <bob181219@yahoo.co.jp>
2018-05-12 12:11:22 +0900

    Describe CommandProcess method in CommandProcessor class

---
+++ soso_cmd/CommandProcessor.java
@@ -0,0 +1,94 @@
+// Copyright 2018 Daiki Yoshida. All rights reserved.
+//
+// This file is part of Soso-CommandLineShell.
+//
+// Soso-CommandLineShell is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Soso-CommandLineShell is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Soso-CommandLineShell. If not, see <http://www.gnu.org/licenses/>.
+
+package soso_cmd;
+
+private class CommandProcessor {
+	public static void CommandProcess(String[] args) throws soso_cmd.Exception {
+		switch(args[0]) {
+		case "mkfile":
+			command_mkfile(args[1]);
+			break;
+
+		case "rmfile":
+			command_rmfile(args[1]);
+			break;
+
+		case "cpfile":
+			command_cpfile(args[1], args[2]);
+			break;
+
+		case "mkdir":
+			command_mkdir(args[1]);
+			break;
+
+		case "rmdir":
+			command_rmdir(args[1]);
+			break;
+
+		case "cpdir":
+			command_cpdir(args[1]);
+			break;
+
+		case "list":
+			if(args.length == 1) {
+				command_list(".");
+			} else {
+				command_list(args[1]);
+			}
+			break;
+
+		case "tview":
+			command_tview(args[1]);
+			break;
+
+		case "bview":
+			command_bview(args[1]);
+			break;
+
+		case "app":
+			String[] appArgs = new String[args.length - 1];
+			for(int i = 1; i < args.length; ++i) {
+				appArgs[i - 1] = args[i];
+			}
+			command_app(appArgs);
+			break;
+
+		case "path":
+			switch(args[1]) {
+			case "add":
+				command_path_add(args[2]);
+				break;
+
+			case "del":
+				command_path_del(args[2]);
+				break;
+
+			case "clear":
+				command_path_clear();
+				break;
+
+			default:
+				throw new soso_cmd.Exception("unknown option of path command");
+			}
+			break;
+
+		default:
+			throw new soso_cmd.Exception("unknown command");
+		}
+	}
+}
